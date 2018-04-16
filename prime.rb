2790cbc0eaa4623f611f8e8e1872ca20b6db2424
Felice Forby <feliceforby@gmail.com>
2018-04-16 23:40:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    false
+  else
+    sqrt = Math.sqrt(number).floor
+    divisors = (2..sqrt).to_a
+    if divisors.any?{ |divisor| number % divisor == 0 }
+      false
+    else
+      true
+    end
+  end
+end
