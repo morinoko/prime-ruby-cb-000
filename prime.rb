f120cd9889ca1e978bab52b760a6612678647a3e
Felice Forby <feliceforby@gmail.com>
2018-04-17 22:35:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,6 @@
+require 'benchmark'
+
+# Using .any? Tends to benchmark faster with large numbers
 def prime?(number)
   if number <= 1 # return false if number is a negative, 0, or 1
     false
@@ -12,3 +15,15 @@ def prime?(number)
     end
   end
 end
+
+# Using .all? (tends to benchmark slower with large numbers)
+# def prime?(number)
+#   if number <= 1 # return false if number is a negative, 0, or 1
+#     false
+#   else
+#     # possible factors range from 2 to the square root of the number (rounded down to the nearest whole number)
+#     sqrt = Math.sqrt(number).floor
+#     factors = (2..sqrt).to_a
+#     factors.all?{ |factor| number % factor != 0 }
+#   end
+# end
